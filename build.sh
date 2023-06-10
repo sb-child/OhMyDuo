d6c564c8875414d6c7592a983ecdcbc1b7f54bb2
sbchild <sbchild0@gmail.com>
2023-06-10 19:30:57 +0800

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 gf version || exit 1
 
-rm internal/packed/data.go
+rm internal/packed/build-pack-data.go
 
-gf build -ps resource -pd internal/packed/data.go -n oh-my-duo-bin
+gf build -n oh-my-duo-bin
+
+cp ./temp/v1.0.0/linux_amd64/oh-my-duo-bin oh-my-duo-linux-amd64
+cp ./temp/v1.0.0/windows_amd64/oh-my-duo-bin.exe oh-my-duo-windows-amd64.exe
